d854529be1e1dbd12716431b7f2cfe976f21fe1b
Pavel Hanzl <hanzlpavel77@gmail>
2024-02-06 19:58:22 +0100

    UI changes

--- src/DataList.js
+++ src/DataList.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import moment from 'moment'; // Importujte knihovnu moment
 
 function DataList() {
     const [data, setData] = useState([]);
-    const [threshold, setThreshold] = useState(25); // Nastavení počáteční threshold
-
+    const [threshold, setThreshold] = useState(35); // Nastavení počáteční threshold
+    const [minutesFilter, setMinutesFilter] = useState(5); // Počáteční hodnota pro filtrování minut
     useEffect(() => {
         // Funkce pro načtení dat
         const fetchData = () => {
@@ -27,17 +28,49 @@ function DataList() {
         setThreshold(Number(event.target.value));
     };
 
+    const handleMinutesFilterChange = (event) => {
+        setMinutesFilter(Number(event.target.value));
+    };
+
+    const getCurrentDateTime = () => moment(); // Aktuální datum a čas
+
     return (
 
         <div>
+            <div class="row">
+                <div class="col-sm ">
 
-            <div className="form-group w-50 p-3 mx-auto">
+
+            <div className="form-group w-50 p-3 float-end">
                 <label htmlFor="thresholdInput">Set maximum of used memory</label>
-                <input type="number" className="form-control" id="thresholdInput" aria-describedby="thresholdInputHelp"
-                       placeholder="Enter email" value={threshold} onChange={handleThresholdChange}/>
-                <small id="thresholdInputHelp" className="form-text text-muted">Application will automatically update values in table below after editing threshold.</small>
+                <input
+                    type="number"
+                    className="form-control"
+                    id="thresholdInput"
+                    aria-describedby="thresholdInputHelp"
+                    placeholder="Enter email"
+                    value={threshold}
+                    onChange={handleThresholdChange}
+                />
+                <small id="thresholdInputHelp" className="form-text text-muted">Filters data with value less than specified.</small>
             </div>
-
+                </div>
+                <div class="col-sm">
+            <div className="form-group w-50 p-3 float-start">
+                <label htmlFor="minutesFilterInput">Set time filter (minutes)</label>
+                <input
+                    type="number"
+                    className="form-control"
+                    id="minutesFilterInput"
+                    aria-describedby="minutesFilterInputHelp"
+                    placeholder="Enter minutes"
+                    value={minutesFilter}
+                    onChange={handleMinutesFilterChange}
+                />
+                <small id="minutesFilterInputHelp" className="form-text text-muted">Filter data for the last {minutesFilter} minutes.</small>
+            </div>
+                </div>
+            </div>
             <table className="table">
                 <thead>
                 <tr>
@@ -46,19 +79,32 @@ function DataList() {
                     <th>Used Memory</th>
                     <th>Free Memory</th>
                     <th>Total Memory</th>
+                    <th>Created At</th>
                 </tr>
                 </thead>
                 <tbody>
-                {data.filter(item => item.usedMemory < threshold).map((item, index) => (
-
-                    <tr key={index}>
-                        <td>{item.id}</td>
-                        <td>{item.clientName.clientName}</td>
-                        <td>{item.usedMemory} MB</td>
-                        <td>{item.freeMemory} MB</td>
-                        <td>{item.totalMemory} MB</td>
-                    </tr>
-                ))}
+                {data
+                    .filter((item) => item.usedMemory < threshold && getCurrentDateTime().diff(moment(item.createdAt), 'minutes') <= minutesFilter) // Filtrování za posledních X minut
+                    .map((item, index) => (
+                        <tr key={index}>
+                            <td className="align-middle">{item.id}</td>
+                            <td className="align-middle">{item.clientName.clientName}</td>
+                            <td className="align-middle">{item.usedMemory} MB</td>
+                            <td className="align-middle">{item.freeMemory} MB</td>
+                            <td className="align-middle">{item.totalMemory} MB</td>
+                            <td>
+                                <div>
+                                    <strong>{moment(item.createdAt).format('HH:mm:ss')}</strong>
+                                </div>
+                                <div>
+                                    <small className="text-secondary">
+                                        {moment(item.createdAt).format('DD-MM-YYYY')}
+                                    </small>
+                                </div>
+                            </td>
+                            {/* Formátování data a času */}
+                        </tr>
+                    ))}
                 </tbody>
             </table>
         </div>
